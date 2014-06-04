c476d6e7795d0937e2f8cfd5dcb6eeb0db36fea5
Shubhojyoti Bhattacharya <shubhojyoti.b@gmail.com>
2014-06-04 13:04:35 +0530

    Exploratory Data Analysis - Course Project 1

---
+++ plot1.R
@@ -0,0 +1,17 @@
+library(datasets)
+zipFileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (file.exists("powerconsumption.zip")) {
+	download.file(zipFileUrl, destfile="powercomsumption.zip", method="curl")
+	unzip("powercomsumption.zip")
+}
+data <- read.table('household_power_consumption.txt', sep=';', header=TRUE)
+data[["Date"]] <- as.Date(data[["Date"]], format="%d/%m/%Y")
+datasub <- subset(data, Date>="2007-02-01" & Date <="2007-02-02")
+GlobalActivePower <- as.numeric(as.character(datasub$Global_active_power))
+hist(GlobalActivePower, col="#fe2500", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.copy(png, "plot1.png", width=480, height=480, units="px")
+dev.off()
+rm(data)
+rm(datasub)
+rm(GlobalActivePower)
+rm(zipFileUrl)
---
+++ plot2.R
@@ -0,0 +1,20 @@
+library(datasets)
+zipFileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (file.exists("powerconsumption.zip")) {
+	download.file(zipFileUrl, destfile="powercomsumption.zip", method="curl")
+	unzip("powercomsumption.zip")
+}
+data <- read.table('household_power_consumption.txt', sep=';', header=TRUE)
+data$DateTime <- paste(data$Date, data$Time, sep=' ')
+data[["Date"]] <- as.Date(data[["Date"]], format="%d/%m/%Y")
+datasub <- subset(data, Date>="2007-02-01" & Date <="2007-02-02")
+datasub[["DateTime"]] <- strptime(datasub[["DateTime"]], format="%d/%m/%Y %H:%M:%S")
+GlobalActivePower <- as.numeric(as.character(datasub$Global_active_power))
+plot(datasub[["DateTime"]], GlobalActivePower, xlab="", ylab="Global Active Power (kilowatts)", type="n")
+lines(datasub[["DateTime"]], GlobalActivePower)
+dev.copy(png, "plot2.png", width=480, height=480, units="px", bg="white", col="black")
+dev.off()
+rm(data)
+rm(datasub)
+rm(GlobalActivePower)
+rm(zipFileUrl)
